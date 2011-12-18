596c5805cdd5c2c2efcbdb82d69f57bb860a1e58
rvm <rvm@fd4b1137-b938-0410-b384-8fdad0efb714>
2011-12-18 15:55:33 +0000

    Reimplemented the option "Show tag info in window title" (preferences -> advanced).
    git-svn-id: https://smplayer.svn.sourceforge.net/svnroot/smplayer/smplayer/trunk@3642 fd4b1137-b938-0410-b384-8fdad0efb714
    
    Conflicts:
    
    	src/basegui.cpp
    	src/prefadvanced.cpp

--- src/basegui.cpp
+++ src/basegui.cpp
@@ -2804,7 +2804,7 @@ void BaseGui::updateMediaInfo() {
 		if (file_dialog->isVisible()) setDataToFileProperties();
 	}
 
-	setWindowCaption( core->mdat.displayName() + " - SMPlayer2" );
+	setWindowCaption( core->mdat.displayName(pref->show_tag_in_window_title) + " - SMPlayer2" );
 
 	emit videoInfoChanged(core->mdat.video_width, core->mdat.video_height, core->mdat.video_fps.toDouble());
 }
